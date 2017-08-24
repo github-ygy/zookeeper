6a99a95ca7f7bf53a12856223c8a4cfeddf1151a
wb-ygy260316 <wb-ygy260316@alibaba-inc.com>
2017-08-24 18:37:52 +0800

    zookeeper  init

---
+++ zookeeper-start/src/main/java/test/ygy/ZookeeperStartApplication.java
@@ -0,0 +1,12 @@
+package test.ygy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookeeperStartApplication.class, args);
+	}
+}
---
+++ zookeeper-start/src/main/java/test/ygy/alone/ZookeeperAloneTest.java
@@ -0,0 +1,12 @@
+package test.ygy.alone;
+
+/**
+ * Created by guoyao on 2017/8/24.
+ */
+public class ZookeeperAloneTest {
+
+    public static void main(String[] args) {
+
+
+    }
+}
